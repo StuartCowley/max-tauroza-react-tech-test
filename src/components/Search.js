19024b9a9f2ffeab71835333ada2605805687d47
Max Tauroza <maxtauroza@gmail.com>
2023-06-26 15:00:50 +0100

    Create getImages.js method

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import '../styles/Search.css';
+import getImages from '../requests/getImages';
 
 const Search = ({ setSearchResults }) => {
   const [value, setValue] = useState('');
 
   const handleInputChange = (e) => setValue(e.target.value);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSearchResults(await getImages(value));
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           className="searchBar"
           type="text"
